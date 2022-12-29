88e4fc9e89f84c16db305ef6fea3a8c67865b3cb
大澤歩夢 <ayumu-osawa@oosawaayumiyumenoMacBook-Pro.local>
2022-12-29 14:32:11 +0900

    Add routing test

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -15,3 +15,11 @@ class TopPageViewTest(TestCase):
         request = HttpRequest()
         response = top(request)
         self.assertEqual(response.content, b"Hello World")
+
+    def test_top_routing_by_status(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_routing_by_content(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content, b"Hello World")
