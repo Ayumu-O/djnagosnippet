ba2dba350f631588e0f20342679e6bf816bc702a
ayumu <ayumu4bus@gmail.com>
2022-12-29 14:57:30 +0900

    Fix CRUD routing tests

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -27,15 +27,15 @@ class TopPageViewTest(TestCase):
 
 class CreateSnippetTest(TestCase):
     def test_should_resolve_snippet_new(self):
-        found = resolve("/snippets/new")
+        found = resolve("/snippets/new/")
         self.assertEqual(snippet_new, found.func)
 
 class SnippetDetailTest(TestCase):
     def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1")
+        found = resolve("/snippets/1/")
         self.assertEqual(snippet_detail, found.func)
 
 class EditSnippetTest(TestCase):
     def test_should_resolve_snippet_edit(self):
-        found = resolve("/snippets/1/edit")
+        found = resolve("/snippets/1/edit/")
         self.assertEqual(snippet_edit, found.func)
