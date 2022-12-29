8e511808d38ed25a5b7864a7d37c3a6452bc67cf
ayumu <ayumu-osawa@oosawaayumiyumenoMacBook-Pro.local>
2022-12-29 14:47:15 +0900

    Add CRUD views and tests

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.http import HttpRequest
+from django.urls import resolve
 
-from snippets.views import top
+from snippets.views import *
 
 # Create your tests here.
 
@@ -23,3 +24,18 @@ class TopPageViewTest(TestCase):
     def test_top_routing_by_content(self):
         response = self.client.get("/")
         self.assertEqual(response.content, b"Hello World")
+
+class CreateSnippetTest(TestCase):
+    def test_should_resolve_snippet_new(self):
+        found = resolve("/snippets/new")
+        self.assertEqual(snippet_new, found.func)
+
+class SnippetDetailTest(TestCase):
+    def test_should_resolve_snippet_detail(self):
+        found = resolve("/snippets/1")
+        self.assertEqual(snippet_detail, found.func)
+
+class EditSnippetTest(TestCase):
+    def test_should_resolve_snippet_edit(self):
+        found = resolve("/snippets/1/edit")
+        self.assertEqual(snippet_edit, found.func)
