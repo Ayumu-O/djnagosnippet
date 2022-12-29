455d864d0bc8fc45080a94768b1d099a020cb117
ayumu <ayumu4bus@gmail.com>
2022-12-29 16:46:19 +0900

    Make detail page test and template

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -10,27 +10,6 @@ UserModel = get_user_model()
 
 # Create your tests here.
 
-"""
-class TopPageViewTest(TestCase):
-    def test_top_returns_200(self):
-        request = HttpRequest()
-        response = top(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_top_returns_expected_content(self):
-        request = HttpRequest()
-        response = top(request)
-        self.assertEqual(response.content, b"Hello World")
-
-    def test_top_routing_by_status(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_top_routing_by_content(self):
-        response = self.client.get("/")
-        self.assertEqual(response.content, b"Hello World")
-"""
-
 class TopPageTest(TestCase):
     def test_top_page_returns_200_and_expected_title(self):
         response = self.client.get("/")
@@ -66,17 +45,62 @@ class TopPageRenderSnippetsTest(TestCase):
         response = top(request)
         self.assertContains(response, self.user.username)
 
+class SnippetDetailTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username='test_user',
+            email='test@example.com',
+            password='secret'
+        )
+        self.snippet = Snippet.objects.create(
+            title='タイトル',
+            code="コード",
+            description="解説",
+            created_by=self.user,
+        )
+
+    def test_should_use_expected_template(self):
+        response = self.client.get(f'/snippets/{self.snippet.id}/')
+        self.assertTemplateUsed(response, 'snippets/snippet_detail.html')
+
+    def test_detail_page_retunrs_200_and_expected_heading(self):
+        response = self.client.get(f'/snippets/{self.snippet.id}/')
+        self.assertContains(response, self.snippet.title, status_code=200)
+
 class CreateSnippetTest(TestCase):
     def test_should_resolve_snippet_new(self):
         found = resolve("/snippets/new/")
         self.assertEqual(snippet_new, found.func)
 
-class SnippetDetailTest(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1/")
-        self.assertEqual(snippet_detail, found.func)
-
 class EditSnippetTest(TestCase):
     def test_should_resolve_snippet_edit(self):
         found = resolve("/snippets/1/edit/")
         self.assertEqual(snippet_edit, found.func)
+
+"""
+class TopPageViewTest(TestCase):
+    def test_top_returns_200(self):
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.content, b"Hello World")
+
+    def test_top_routing_by_status(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_routing_by_content(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content, b"Hello World")
+"""
+
+"""
+class SnippetDetailTest(TestCase):
+    def test_should_resolve_snippet_detail(self):
+        found = resolve("/snippets/1/")
+        self.assertEqual(snippet_detail, found.func)
+"""
--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from snippets.models import Snippet
@@ -17,4 +17,6 @@ def snippet_edit(request, snippet_id):
     return HttpResponse('スニペットの編集')
 
 def snippet_detail(request, snippet_id):
-    return HttpResponse('スニペットの詳細閲覧')
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    context = {'snippet': snippet}
+    return render(request, 'snippets/snippet_detail.html', context)
