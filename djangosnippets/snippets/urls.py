f7e3616de7a5678fc85d1c80a9f907bb211aa9c5
ayumu <ayumu4bus@gmail.com>
2023-01-02 15:05:54 +0900

    Add delete func

--- djangosnippets/snippets/urls.py
+++ djangosnippets/snippets/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('new/', snippet_new, name='snippet_new'),
     path('<int:snippet_id>/', snippet_detail, name='snippet_detail'),
     path('<int:snippet_id>/edit/', snippet_edit, name='snippet_edit'),
+    path('<int:snippet_id>/delete/', snippet_delete, name='snippet_delete'),
 ]
--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -42,6 +42,15 @@ def snippet_edit(request, snippet_id):
     return render(request, 'snippets/snippet_edit.html', {'form': form})
 
 @require_safe
+@login_required
+def snippet_delete(request, snippet_id):
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    if snippet.created_by_id != request.user.id:
+        return HttpResponseForbidden('このスニペットの削除は許可されていません')
+    snippet.delete()
+    return redirect('top')
+
+@require_safe
 def snippet_detail(request, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     context = {'snippet': snippet}
