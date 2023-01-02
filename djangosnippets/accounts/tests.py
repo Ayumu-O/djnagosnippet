f70adc69118b2c56b77855cd540d8450a31c9687
ayumu <ayumu4bus@gmail.com>
2023-01-02 10:13:02 +0900

    Show username on header

--- djangosnippets/accounts/tests.py
+++ djangosnippets/accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
 
 # Create your tests here.
+
+class UserAuthentificationTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username='test_user',
+            email='test@example.com',
+            password='secret'
+        )
+        self.client.force_login(self.user)
+
+    def test_render_username(self):
+        response = self.client.get('/')
+        self.assertContains(response, self.user.username, status_code=200)
