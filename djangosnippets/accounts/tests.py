5ff498f14a7d2f7987f18810b79cd5b5d8599d23
ayumu <ayumu4bus@gmail.com>
2023-01-08 14:40:52 +0900

    Change to login automaticaly after signup

--- djangosnippets/accounts/tests.py
+++ djangosnippets/accounts/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, get_user
+from django.urls import reverse
 
 UserModel = get_user_model()
 
 # Create your tests here.
 
-class UserAuthentificationTest(TestCase):
+class UserAuthenticationTest(TestCase):
     def setUp(self):
         self.user = UserModel.objects.create(
             username='test_user',
@@ -17,3 +18,17 @@ class UserAuthentificationTest(TestCase):
     def test_render_username(self):
         response = self.client.get('/')
         self.assertContains(response, self.user.username, status_code=200)
+
+class SignUpTest(TestCase):
+    def setUp(self):
+        self.url = reverse('signup')
+        self.data = {
+            'username': 'testtest',
+            'password1': 'abcde123456',
+            'password2': 'abcde123456',
+        }
+
+    def test_should_login_after_signup(self):
+        self.assertFalse(get_user(self.client).is_authenticated)
+        self.client.post(self.url, self.data)
+        self.assertTrue(get_user(self.client).is_authenticated)
--- djangosnippets/accounts/views.py
+++ djangosnippets/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        to_return = super().form_valid(form)
+        user = authenticate(
+            username=form.cleaned_data["username"],
+            password=form.cleaned_data["password1"]
+        )
+        login(self.request, user)
+        return to_return
